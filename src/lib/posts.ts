7393c4d6f40bf44ecf647ae8a678b6a622381ac3
AiFahri <fahrinurilll@gmail.com>
2025-10-26 03:08:57 +0700

    initial commit

---
+++ src/lib/posts.ts
@@ -0,0 +1,24 @@
+export type Post = {
+  slug: string;
+  title: string;
+  summary: string;
+  image?: string;
+  date: string;
+};
+
+export const posts: Post[] = [
+  {
+    slug: "optimasi-metadata-nextjs",
+    title: "Optimasi Metadata di Next.js",
+    summary: "Cara pakai metadata dan og image agar ramah SEO",
+    image: "/blog/optimasi.jpg",
+    date: "2025-10-25",
+  },
+  {
+    slug: "seo-dasar",
+    title: "SEO Dasar Untuk Pemula",
+    summary: "Cara mengoptimalkan SEO pada website anda supaya jadi yg teratas",
+    image:"/blog/seo.jpg",
+    date: "2025-10-25",
+  }
+];  
