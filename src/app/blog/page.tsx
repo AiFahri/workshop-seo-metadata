3825e09143ccab7a7f251b42ab22d8979ed5ad36
AiFahri <fahrinurilll@gmail.com>
2025-11-05 07:56:52 +0700

    feat: create BlogPage component with metadata for improved SEO

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,12 @@
+
+ import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Blog Page",
+  description: "This is the blog page description",
+};
+
+const BlogPage = () => {
+  return <div>Blog Page</div>;
+}
+export default BlogPage;
